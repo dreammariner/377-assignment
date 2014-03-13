f06311368f1f3e4b45dc4f372e666427fd788f51
Mike Herbert <mjherber@student.umass.edu>
2014-03-13 18:05:58 -0400

    Number of mapper/reducer threads
    CL parameters

--- Index.java
+++ Index.java
@@ -1,14 +1,18 @@
 //Dan Gereb
 //Michael Herbert 
+
+/*
+ * str.replaceAll("[^A-Za-z0-9 ]").toLowerCase()
+ * Convert all words to lowercase while ignoring non-alphanumeric characters
+*/
+
 public class Index {
 	public int m = 0; //shared counter variable m
 	
 	public static void main(String[] args){
-		
-		/*
-		 * str.replaceAll("[^A-Za-z0-9 ]").toLowerCase()
-		 * Convert all words to lowercase while ignoring non-alphanumeric characters
-		 */
+		int numOfReducer = Integer.parseInt(args[0]); //first command-line parameter
+		int numOfMapper = Integer.parseInt(args.length - 1); //number of command-line parameters, ignoring the first
+
 		
 	}
 }
