b5633cb09d5bc8cae8c9cef9ce37c86666fe93ca
Mike Herbert <mjherber@student.umass.edu>
2014-03-13 18:34:39 -0400

    Adding to GROUP/README.txt, ignore changes to Index.java
    Testing things, work-in-progress, edit as you see fit

--- Index.java
+++ Index.java
@@ -1,6 +1,8 @@
 //Dan Gereb
 //Michael Herbert 
 
+import java.util.concurrent.Semaphore; //using Semaphore
+
 /*
  * str.replaceAll("[^A-Za-z0-9 ]").toLowerCase()
  * Convert all words to lowercase while ignoring non-alphanumeric characters
@@ -8,11 +10,17 @@
 
 public class Index {
 	public int m = 0; //shared counter variable m
+	public Semaphore sem = new Semaphore(1);
 	
 	public static void main(String[] args){
+		
 		int numOfReducer = Integer.parseInt(args[0]); //first command-line parameter
 		int numOfMapper = Integer.parseInt(args.length - 1); //number of command-line parameters, ignoring the first
 
+		for(int i = 1; i < numOfMapper; i++){
+			//Map thread open file at args[i]
+			//str.replaceAll("[^A-Za-z0-9 ]").toLowerCase() //Convert all words to lowercase while ignoring non-alphanumeric characters
+		}
 		
 	}
 }
---
+++ README.txt
@@ -0,0 +1,4 @@
+Michael Herbert
+Dan Gereb
+
+We began by setting up a GitHub repository to allow our collaboration to extend beyond in person meet ups, due to conflicting schedules. Once the skeleton of the project was laid out, we split up different areas to research and return with. Dan volunteered to look into implementing our own Bounded Buffer while Michael decided to explore how semaphores are implemented in Java. 
