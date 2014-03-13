21785b024d696b20bc2ec5cc07fc0b8f81ffafe1
Mike Herbert <mjherber@student.umass.edu>
2014-03-13 17:59:45 -0400

    Started adding code based on implementation specs
    Created main method.

--- Index.java
+++ Index.java
@@ -1,4 +1,14 @@
-
+//Dan Gereb
+//Michael Herbert 
 public class Index {
-
+	public int m = 0; //shared counter variable m
+	
+	public static void main(String[] args){
+		
+		/*
+		 * str.replaceAll("[^A-Za-z0-9 ]").toLowerCase()
+		 * Convert all words to lowercase while ignoring non-alphanumeric characters
+		 */
+		
+	}
 }
