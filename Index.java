b5d1311f617bc66409a1554ac6bcd2ac96c42c14
Mike Herbert <mjherber@student.umass.edu>
2014-03-15 01:50:59 -0400

    Comments/txt file

--- Index.java
+++ Index.java
@@ -11,17 +11,16 @@ public class Index {
 	public int m = 0; //shared counter variable m
 	public Semaphore sem = new Semaphore(1);
 	
-	
-	
+
 	public static void main(String[] args){
 		
 		int numOfReducer = Integer.parseInt(args[0]); //first command-line parameter
 		int numOfMapper = Integer.parseInt(args.length - 1); //number of command-line parameters, ignoring the first
 		//using a LinkedList as our BoundedBuffer
 		for(int i = 1; i < numOfMapper; i++){
-			Thread mapThread = new Thread(new Map(args[i]));
-
+			Thread mapThread = new Thread(new Map(args[i])); //new thread, passing in file names
 		}
+		
 		for(int i = 0; i < numOfReducer; i++){
 			Thread reduceThread = new Thread(new Reduce());
 		}
