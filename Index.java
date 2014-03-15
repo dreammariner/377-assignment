10f34865ac395e8b4278f76209f76148072d51cd
dreammariner <dannygereb@gmail.com>
2014-03-15 00:40:25 -0400

    more progress...

--- Index.java
+++ Index.java
@@ -12,30 +12,6 @@ public class Index {
 	public Semaphore sem = new Semaphore(1);
 	
 	
-	public void readFile(String file){
-		int lineNumber = 0;
-		String currentLine = null;
-		
-		try  
-		{  
-			FileReader fr = new FileReader(file);  
-			BufferedReader myReader = new BufferedReader(fr);
-		}
-		catch(IOException e){
-			System.out.println("IO Exception!");
-		}
-		while (myReader.readLine() != null) {
-			lineNumber++;
-	        currentLine = myReader.readLine();
-	        currentLine.replaceAll("[^A-Za-z0-9 ]").toLowerCase();
-	        String[] lineWords = currentLine.split(" ");
-	        /*
-	         * Line is now split up into seperate words.
-	         */
-	        //Iterate through lineWords, add lineWords[current index], lineNumber to queue
-		}
-	}
-
 	
 	public static void main(String[] args){
 		
--- Map.java
+++ Map.java
@@ -18,6 +18,29 @@ class Map implements Runnable{
 	 * 
 	 */
 	public void run(){ 
+		
+		int lineNumber = 0;
+		String currentLine = null;
+		
+		try  
+		{  
+			FileReader fr = new FileReader(file);  
+			BufferedReader myReader = new BufferedReader(fr);
+		}
+		catch(IOException e){
+			System.out.println("IO Exception!");
+		}
+		while (myReader.readLine() != null) {
+			lineNumber++;
+	        currentLine = myReader.readLine();
+	        currentLine.replaceAll("[^A-Za-z0-9 ]").toLowerCase();
+	        String[] lineWords = currentLine.split(" ");
+	        /*
+	         * Line is now split up into seperate words.
+	         */
+	        //Iterate through lineWords, add lineWords[current index], lineNumber to queue
+		}
+	}
 		synchronized(m){
 			m++;
 		}
